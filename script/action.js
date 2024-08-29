dc4d4c7d94439f664eb3f03e5d888be952461fd2
euihyuck <euihyk12@naver.com>
2024-08-29 10:55:33 +0900

    1.3

--- script/action.js
+++ script/action.js
@@ -1,30 +1,38 @@
 $('header nav .gnb li').click(function () {
-    $(this).addClass('on').siblings().removeClass('on')
+    $(this).addClass('on').siblings().removeClass('on');
+
+    let liIndex = $(this).index() //0,1,2,3
+    let sectionTop = $('#papa > div').eq(liIndex).offset().top;
+
+    $('html, body').animate({scrollTop: sectionTop})
+
+    return false
+
 })
 
-$('header nav li').eq(1).click(function () {
-    $('html').animate({ scrollTop: '1300px' })
-})
+// $('header nav li').eq(1).click(function () {
+//     $('html').animate({ scrollTop: '1300px' })
+// })
 
-$('header nav li').eq(3).click(function () {
-    $('html').animate({ scrollTop: '2600px' })
-}) 
+// $('header nav li').eq(3).click(function () {
+//     $('html').animate({ scrollTop: '2600px' })
+// }) 
 
-$('header nav li').eq(4).click(function () {
-    $('html').animate({ scrollTop: '5200px' })
-}) 
+// $('header nav li').eq(4).click(function () {
+//     $('html').animate({ scrollTop: '5200px' })
+// }) 
 
 
 
 $('#section3 .click').click(function(){
-    $('#section3 figure').hide(1000)
-    $('#section3 .img_box').css({opacity:'1', visibility:'visible',transition:'1s'})
-    $('#section3 .close').css({opacity:'1', visibility:'visible',transition:'0.5s'})
+    $('#section3 figure').stop().hide()
+    $('#section3 .img_box').stop().fadeIn(300).css({display:'flex'})
+    $('#section3 .close').stop().fadeIn(300).css({display:'flex'})
 })
 
-$('#section3 .close').dblclick (function(){
-    $(this).css({opacity:'0', visibility:'hidden',transition:'1s'})   
-    $('#section3 .img_box').css({opacity:'0', visibility:'hidden',transition:'1s'}) 
-    $('#section3 figure').show(1000)
+$('#section3 .close').click(function(){
+    $(this).stop().fadeOut(300)
+    $('#section3 .img_box').stop().fadeOut(300)
+    $('#section3 figure').fadeIn()
     
 })
